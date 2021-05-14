e984ccb0683dc17283a0d73c79ea754e228d30ce
fredsalas87 <fredsalas87@gmail.com>
2021-05-14 16:18:10 -0300

    Update app

--- weather_app.py
+++ weather_app.py
@@ -8,12 +8,16 @@ def pegarCoordenadas():
 
     if (r.status_code != 200):
         print('Não foi possível obter a localização')
+        return None
     else:
-        localizacao = json.loads(r.text)
-        coordenadas = {}
-        coordenadas['lat'] = localizacao['geoplugin_latitude']
-        coordenadas['long'] = localizacao['geoplugin_longitude']
-        return coordenadas
+        try:
+            localizacao = json.loads(r.text)
+            coordenadas = {}
+            coordenadas['lat'] = localizacao['geoplugin_latitude']
+            coordenadas['long'] = localizacao['geoplugin_longitude']
+            return coordenadas
+        except:
+            return None
 
 def pegarCodigoLocal(lat, long):
     locationAPIUrl = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition' \
@@ -23,14 +27,18 @@ def pegarCodigoLocal(lat, long):
     r = requests.get(locationAPIUrl)
     if (r.status_code != 200):
         print('Não foi possível obter o código do local')
+        return None
     else:
-        locationResponse = json.loads(r.text)
-        infoLocal = {}
-        infoLocal['nomeLocal'] = locationResponse['LocalizedName'] + ', '\
-                    + locationResponse['AdministrativeArea']['LocalizedName'] + '. '\
-                    + locationResponse['Country']['LocalizedName']
-        infoLocal['codigoLocal'] = locationResponse['Key']
-        return infoLocal
+        try:
+            locationResponse = json.loads(r.text)
+            infoLocal = {}
+            infoLocal['nomeLocal'] = locationResponse['LocalizedName'] + ', '\
+                        + locationResponse['AdministrativeArea']['LocalizedName'] + '. '\
+                        + locationResponse['Country']['LocalizedName']
+            infoLocal['codigoLocal'] = locationResponse['Key']
+            return infoLocal
+        except:
+            return None
 
 def pegarTempoAgora(codigoLocal, nomeLocal):
     currentConditionsAPIUrl = 'http://dataservice.accuweather.com/currentconditions/v1/' \
@@ -39,22 +47,27 @@ def pegarTempoAgora(codigoLocal, nomeLocal):
     r = requests.get(currentConditionsAPIUrl)
     if (r.status_code != 200):
         print('Não foi possível obter o clima do local')
+        return None
     else:
-        currentConditionsAPIUrl = json.loads(r.text)
-        infoClima = {}
-        infoClima['textoClima'] = currentConditionsAPIUrl[0]['WeatherText']
-        infoClima['temperatura'] = currentConditionsAPIUrl[0]['Temperature']['Metric']['Value']
-        infoClima['nomeLocal'] = nomeLocal
-    return infoClima
-
+        try:
+            currentConditionsAPIUrl = json.loads(r.text)
+            infoClima = {}
+            infoClima['textoClima'] = currentConditionsAPIUrl[0]['WeatherText']
+            infoClima['temperatura'] = currentConditionsAPIUrl[0]['Temperature']['Metric']['Value']
+            infoClima['nomeLocal'] = nomeLocal
+            return infoClima
+        except:
+            return None
 ## Inicio do programa
 
-coordenadas = pegarCoordenadas()
+try:
+    coordenadas = pegarCoordenadas()
+    local = pegarCodigoLocal(coordenadas['lat'], coordenadas['long'])
+    climaAtual = pegarTempoAgora(local['codigoLocal'], local['nomeLocal'])
+    print('Clima atual em: ' + climaAtual['nomeLocal'])
+    print(climaAtual['textoClima'])
+    print('Temperatura: ' + str(climaAtual['temperatura']) + '\xb0' + 'C')
+except:
+    print('Não foi possível obter o clima atual')
 
-local = pegarCodigoLocal(coordenadas['lat'], coordenadas['long'])
 
-climaAtual = pegarTempoAgora(local['codigoLocal'], local['nomeLocal'])
-
-print('Clima atual em: ' + climaAtual['nomeLocal'])
-print(climaAtual['textoClima'])
-print('Temperatura: ' + str(climaAtual['temperatura']) + '\xb0' + 'C')
